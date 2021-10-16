3ca733f490b176247b2edda5c48e6ffd4edbb3cf
DavidHu411 <david.hu@mail.mcgill.ca>
2021-10-16 00:39:18 -0400

    started persistence

---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/AccountRepository.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse321.librarysystem.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.librarysystem.models.Account;
+
+public interface AccountRepository extends CrudRepository<Account, Integer> {
+
+	Account findAccountById(int id);
+
+}
