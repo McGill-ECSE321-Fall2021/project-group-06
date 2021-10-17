6305d1b91a6c11b19d5eeaef4bb36b499c31f3a5
DavidHu411 <david.hu@mail.mcgill.ca>
2021-10-16 22:33:31 -0400

    changed media slightly (trivial changes) and wrote the shift class. (Note: when writing HeadLibrarian, please use the ManyToMany tag with the (mappedBy = HeadLibrarian) since head librarian owns shift. Thank you)

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/models/Shift.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/models/Shift.java
@@ -1,5 +1,72 @@
 package ca.mcgill.ecse321.librarysystem.models;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
+
+
+@Entity
 public class Shift {
-    
+    private int shiftID;
+    public void setShiftID(int shiftID)
+    {
+        this.shiftID = shiftID;
+    }
+
+    @Id
+    public int getShiftID()
+    {
+        return this.shiftID;
+    }
+
+    private Date date;
+    public void setDate(Date date){
+        this.date = date;
+    }
+
+    public Date getDate(){
+        return this.date;
+    }
+
+    private Time startTime;
+    public void setStartTime(Time startTime){
+        this.startTime = startTime;
+    }
+
+    public Time getStartTime(){
+        return this.startTime;
+    }
+
+    private Time endTime;
+    public void setEndTime(Time endTime){
+        this.endTime = endTime;
+    }
+
+    public Time getEndTime(){
+        return this.endTime;
+    }
+
+    private HeadLibrarian headLibrarian;
+    @ManyToOne(optional=false)
+    public HeadLibrarian getHeadLibrarian(){
+        return this.headLibrarian;
+    }
+
+    public void setHeadLibrarian(HeadLibrarian headLibrarian){
+        this.headLibrarian = headLibrarian;
+    }
+
+    private Set<Librarian> librarians;
+    @ManyToMany
+    public Set<Librarian> getLibrarians(){
+        return this.librarians;
+    }
+
+    public void setLibrarians(Set<Librarian> librarians){
+        this.librarians = librarians;
+    }
 }
