420ef9a175512cf2430477284d31af8a3d4dc51b
DavidHu411 <david.hu@mail.mcgill.ca>
2021-10-17 23:13:25 -0400

    fixing so it builds

--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/TestLibrarySystemApplicationService.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/TestLibrarySystemApplicationService.java
@@ -35,7 +35,7 @@ import ca.mcgill.ecse321.librarysystem.models.Account.AccountCategory;
 
 
 @ExtendWith(MockitoExtension.class)
-public class TestLibrarySystemApplicationPersistance {
+public class TestLibrarySystemApplicationService {
 
     @Mock AccountRepository accountRepository;
     @Mock EventRepository eventRepository;
