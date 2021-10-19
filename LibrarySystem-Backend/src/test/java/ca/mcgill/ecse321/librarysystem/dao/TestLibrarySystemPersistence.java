c901df6f5cc59fcb43a4dcb5311e830da8652400
Samuel Lin <lin.sam217@gmail.com>
2021-10-18 21:35:37 -0400

    Added Event & CheckOutItem Persistence Tests

--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestLibrarySystemPersistence.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestLibrarySystemPersistence.java
@@ -85,4 +85,79 @@ public class TestLibrarySystemPersistence {
 //		assertEquals(name, event.getName());
 		
 	}
+	
+	@Test
+	public void testPersistAndLoadCheckOutItem() {
+		
+		// Set up test account to perform operations on CheckOutItem / Media
+		Account acc = new Online();
+		acc.setId(17);
+		accountRepository.save(acc);
+		
+		// Create movie media and set Media details AND CheckOutItem details
+		CheckOutItem movieItem = new CheckOutItem();
+		movieItem.setType(Media.Item.Movie);		// Also verify that CheckOutItem can inherit methods from Media
+		int movieItemID = 71;
+		movieItem.setID(movieItemID);
+		movieItem.setAccount(acc);
+		movieItem.setIsCheckedOut(true);
+		movieItem.setIsReserved(false);
+		int borrowingPeriod = 21;
+		movieItem.setBorrowingPeriod(borrowingPeriod);
+		
+		mediaRepository.save(movieItem); 	// Save it in Media repository
+		
+		// Persistence testing
+		movieItem = null;
+		movieItem = mediaRepository.findMediaById(movieItemID);
+		
+		assertNotNull(movieItem);			// Check movieItem was saved in mediaRepository
+		assertEquals(true, getIsCheckedOut());
+		assertEquals(false, getIsReserved());
+		assertEquals(borrowingPeriod, getBorrowingPeriod());
+	}
+	
+	@Test
+	public void testPersistAndLoadEvent() {
+		// Set up test account to perform operations on Event
+		Account acc = new Online();
+		acc.setId(18);
+		accountRepository.save(acc);
+		
+		// Create Event and set its details
+		Event testEvent = new Event();
+		testEvent.setAccount(acc);
+		String testEventName = "Event 1";
+		Date date = java.sql.Date.valueOf(LocalDate.of(2021, Month.OCTOBER, 31));
+		Time startTime = java.sql.Time.valueOf(LocalTime.of(11, 00));
+		Time endTime = java.sql.Time.valueOf(LocalTime.of(12, 00));
+		testEvent.setName(testEventName);
+		testEvent.setDate(date);
+		testEvent.setEventStart(startTime);
+		testEvent.setEventEnd(endTime);
+		
+		eventRepository.save(testEvent);	// Save it in Event repository
+		
+		// Persistence testing: findEventByName (Event)
+		
+		testEvent = null;
+		testEvent = eventRepository.findEventByName(testEventName);
+		
+		assertNotNull(testEvent);
+		assertEquals(date, testEvent.getDate());
+		assertEquals(startTime, testEvent.getEventStart());
+		assertEquals(endTime, testEvent.getEventEnd());
+		
+		// Persistence testing: findByAccount (List<Event>)
+		testEvent = null;
+		List<Event> listEvent = eventRepository.findByAccount(acc.getId()); 
+		assertNotNull(listEvent);
+		
+		for (Event testEvent: listEvent) {
+			assertNotNull(testEvent);
+			assertEquals(date, testEvent.getDate());
+			assertEquals(startTime, testEvent.getEventStart());
+			assertEquals(endTime, testEvent.getEventEnd());
+		}
+	}
 }
