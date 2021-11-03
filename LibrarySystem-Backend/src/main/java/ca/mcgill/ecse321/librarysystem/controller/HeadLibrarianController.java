097f3d1fb649aebcfc014b12e0edb31e7c326a28
Joyce Liu <joyce.liu2@mail.mcgill.ca>
2021-11-03 18:09:35 -0400

    added methods to see all librarians

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/HeadLibrarianController.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/HeadLibrarianController.java
@@ -1,17 +1,23 @@
 package ca.mcgill.ecse321.librarysystem.controller;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.librarysystem.dto.HeadLibrarianDto;
+import ca.mcgill.ecse321.librarysystem.dto.LibrarianDto;
 import ca.mcgill.ecse321.librarysystem.dto.OpeningHourDto;
 import ca.mcgill.ecse321.librarysystem.dto.ShiftDto;
 import ca.mcgill.ecse321.librarysystem.models.HeadLibrarian;
+import ca.mcgill.ecse321.librarysystem.models.Librarian;
 import ca.mcgill.ecse321.librarysystem.models.OpeningHour;
 import ca.mcgill.ecse321.librarysystem.models.Shift;
 import ca.mcgill.ecse321.librarysystem.models.Account.AccountCategory;
@@ -37,15 +43,44 @@ public class HeadLibrarianController {
 		return convertToDto(head);
 	}
 	
-	HeadLibrarianDto convertToDto(HeadLibrarian h) {
+	@GetMapping(value= {"/headlibrarian/{id}", "/headlibrarian/{id}/"})
+	public HeadLibrarianDto getHeadLibrarianById(@PathVariable("id") int id) throws IllegalArgumentException{
+		return convertToDto(headService.getHeadLibrarian(id));
+	}
+	
+	@GetMapping(value= {"/librarians", "/librarians/"})
+	public List<LibrarianDto> getAllLibrarians(){
+		List<LibrarianDto> librDtos=new ArrayList<>();
+		for (Librarian l : headService.getLibrarians()) {
+			librDtos.add(convertToDto(l));
+		}
+		return librDtos;
+	}
+	//TODO
+	private HeadLibrarianDto convertToDto(HeadLibrarian h) {
 		if (h==null) {
 			throw new IllegalArgumentException("Head Librarian not found.");
 		}
 		HeadLibrarianDto headDto=new HeadLibrarianDto(createOpHourDtosForHead(h), null);
-		//headDto.setShifts(shifts);
-		//shifts is hashSet of ShiftDto
+		/*either:
+		   --> keep above null & headDto.setShifts(shifts) after shiftDto constructor has been established
+		 		(shifts is hashSet of ShiftDto)
+		or --> constructor for shiftDto is null and this one is not with createShiftDtos(h)
+		 		(shiftDto will have to setHeadLibr and setLibrarians
+		*/
 		return headDto;
 	}
+	//TODO
+	private LibrarianDto convertToDto(Librarian l) {
+		if (l==null) {
+			throw new IllegalArgumentException("Librarian not found.");
+		}
+		LibrarianDto lib=new LibrarianDto(null);
+		/*same issue: either createShiftDtos(l) as argument
+		or have shiftDto be established first*/
+		return lib;
+	}
+	
     private HashSet<OpeningHourDto> createOpHourDtosForHead(HeadLibrarian h) {
 		HashSet<OpeningHour> opHourForHead=(HashSet<OpeningHour>) hourService.getOpeningHourList(h);
 		HashSet<OpeningHourDto> opHour=new HashSet<>();
@@ -54,8 +89,7 @@ public class HeadLibrarianController {
 		}
 		return opHour;
 	}
-	
-	//This method bases on the assumption that shiftDto will be done first
+	//TODO
 	/*
 	private HashSet<Shift> createShiftDtos(HeadLibrarian h) {
 		HashSet<Shift> shiftForHead=shiftService.getShifts(h);
--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/HeadLibrarianService.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/HeadLibrarianService.java
@@ -10,7 +10,9 @@ import ca.mcgill.ecse321.librarysystem.models.Account.AccountCategory;
 import ca.mcgill.ecse321.librarysystem.models.HeadLibrarian;
 import ca.mcgill.ecse321.librarysystem.models.Librarian;
 import ca.mcgill.ecse321.librarysystem.models.Shift;
+import javassist.expr.Instanceof;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -112,4 +114,16 @@ public class HeadLibrarianService {
     	eventRepository.deleteAll(eventRepository.findByAccount(firedLibr));
     	accountRepository.delete(firedLibr);
     }
+	
+	@Transactional
+	public List<Librarian> getLibrarians(){
+		List<Account> allAccounts=(List<Account>) accountRepository.findAll();
+		List<Librarian> allLibrarians=new ArrayList<>();
+		for (Account a : allAccounts) {
+			if (a instanceof Librarian){
+				allLibrarians.add((Librarian) a);
+			}
+		}
+		return allLibrarians;
+	}
 }
