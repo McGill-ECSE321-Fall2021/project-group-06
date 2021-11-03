e3bf4b4920a332b2a15c7cd25028ea827277714e
Niels <77604434+ballofsunshine@users.noreply.github.com>
2021-11-02 21:42:27 -0400

    Created OpeningHourController class, OpeningHourService class, Conversion class.
    Created the OpeningHourController and OpeningHourService classes.
    Created a Conversion class in order to convert objects into their DTO version.

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/OpeningHourController.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/OpeningHourController.java
@@ -1,5 +1,36 @@
 package ca.mcgill.ecse321.librarysystem.controller;
 
+import java.sql.Date;
+import java.sql.Time;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.librarysystem.dto.OpeningHourDto;
+import ca.mcgill.ecse321.librarysystem.models.OpeningHour;
+import ca.mcgill.ecse321.librarysystem.service.OpeningHourService;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class OpeningHourController {
-    
+
+    @Autowired
+    private OpeningHourService service;
+
+    @PostMapping(value = { "/openingHours", "/openingHours/"})
+    public OpeningHourDto createOpeningHour(@RequestParam int id, @RequestParam Date date, @RequestParam Time startTime, @RequestParam Time endTime) throws Exception{
+        // OpeningHour openingHour = null;
+        try{
+            OpeningHour openingHour= service.createOpeningHour(id, date, startTime, endTime);
+            return Conversion.convertToDto(openingHour);
+        } catch (IllegalArgumentException e) {
+            throw new Exception(e.getMessage());
+        }        
+    }
 }
