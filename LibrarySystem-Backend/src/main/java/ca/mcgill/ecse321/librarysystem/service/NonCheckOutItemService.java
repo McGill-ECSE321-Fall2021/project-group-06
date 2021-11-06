8b87a5b2b75d96fa9a17615629c30e1bd4717f04
Samuel Lin <lin.sam217@gmail.com>
2021-11-06 14:01:52 -0400

    Updated CheckOutItemService and NonCheckOutItemService
    Only included "create" business methods for both as Media is an abstract class (which includes delete, get, etc.)

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/NonCheckOutItemService.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/NonCheckOutItemService.java
@@ -1,5 +1,61 @@
 package ca.mcgill.ecse321.librarysystem.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.librarysystem.dao.MediaRepository;
+import ca.mcgill.ecse321.librarysystem.models.Media;
+import ca.mcgill.ecse321.librarysystem.models.Media.Item;
+import ca.mcgill.ecse321.librarysystem.models.NonCheckOutItem;
+
+@Service
 public class NonCheckOutItemService {
-    
+
+    @Autowired
+    private MediaRepository mediaRepository;
+
+    /**
+     * 
+     * @param mediaType
+     * @param mediaID
+     * @return media
+     * @author Samuel
+     */
+    @Transactional
+    public Media createNonCheckOutItem (Item mediaType, int mediaID){
+
+        // Input validation (methods from tutorial notes 2.6.1)
+        String error ="";
+
+        if (mediaType == null) {
+            error = error + "Media type not found! (null)";
+        }
+
+        boolean isValidType = false;
+        for (Item item: Item.values()){
+
+            if (mediaType.equals(item)) {
+                isValidType = true;
+                break;
+            }
+        }
+        if (!isValidType){
+            error = error + "Media type invalid!";
+        }
+
+        if (mediaRepository.findMediaByID(mediaID) != null) {
+            error = error + "Media ID already exists!";
+        }
+        error = error.trim();
+        if (error.length() > 0) {
+            throw new IllegalArgumentException(error);
+        }
+
+        NonCheckOutItem nonCheckOutItem = new NonCheckOutItem();
+        nonCheckOutItem.setType(mediaType);
+        nonCheckOutItem.setID(mediaID);
+        mediaRepository.save(nonCheckOutItem);
+        return nonCheckOutItem;
+    }
 }
