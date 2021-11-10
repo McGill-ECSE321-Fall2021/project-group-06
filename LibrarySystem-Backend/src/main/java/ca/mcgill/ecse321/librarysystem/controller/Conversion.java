976e4e6e46f9adcda9d514a1d21a8821bc3725a4
Howard Yu <howard@Howards-MacBook-Air.local>
2021-11-09 19:39:32 -0500

    added some convertToDto as well as fixed some Dto files

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/Conversion.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/Conversion.java
@@ -4,33 +4,67 @@ import ca.mcgill.ecse321.librarysystem.dto.*;
 import ca.mcgill.ecse321.librarysystem.models.*;
 
 public class Conversion {
+
     public static OpeningHourDto convertToDto(OpeningHour openingHour){
         if(openingHour == null) throw new IllegalArgumentException("Opening Hour not found.");
         return new OpeningHourDto(openingHour.getId(), openingHour.getDayOfWeek(), openingHour.getStartTime(), openingHour.getEndTime());
     }
     
-    public static CheckOutItemDto convertToDTO(CheckOutItem checkOutItem) {
+    public static CheckOutItemDto convertToDto(CheckOutItem checkOutItem) {
 		if(checkOutItem==null) throw new IllegalArgumentException("Checkout item not found.");
-		CheckOutItemDto checkOutItemDto = new CheckOutItemDto(checkOutItem.getID(),checkOutItem.getIsCheckedOut(),checkOutItem.getIsReserved(), checkOutItem.getBorrowingPeriod(), checkOutItem.getStartDate());
-        return checkOutItemDto;
-	  }
-    
+		return new CheckOutItemDto(checkOutItem.getID(),checkOutItem.getIsCheckedOut(),checkOutItem.getIsReserved(), checkOutItem.getBorrowingPeriod(), checkOutItem.getStartDate());
+	}
+
+    public static NonCheckOutItemDto convertToDto(NonCheckOutItem nonCheckOutItem) {
+		if(nonCheckOutItem==null) throw new IllegalArgumentException("NonCheckout item not found.");
+        return new NonCheckOutItemDto(nonCheckOutItem.getType(), nonCheckOutItem.getID());
+	}
+
     public static EventDto convertToDto(Event event){
         if(event==null) throw new IllegalArgumentException("Event not found.");
         return new EventDto(event.getName(),event.getDate(), event.getEventStart(),event.getEventEnd());
     }
+    
     public static OfflineDto convertToDto(Offline offline){
         if(offline==null) throw new IllegalArgumentException("Customer not found.");
         
-        HashSet<EventDto> howard = (HashSet)offline.getEvents();
+        HashSet<EventDto> events = new HashSet<EventDto>();
+        HashSet<Event> modelEvents = (HashSet)offline.getEvents();
+        for(Event event: modelEvents){
+            events.add(convertToDto(event));
+        }
 
-        HashSet<MediaDto> howard2 = (HashSet)offline.getMedias();
-        return new OfflineDto(offline.getId(), offline.getAddress(), offline.getName(), offline.getAccountCategory(), offline.getIsLocal(), offline.getNumChecked(), howard, howard2);
+        HashSet<MediaDto> medias = new HashSet<MediaDto>();
+        HashSet<Media> modelMedias = (HashSet)offline.getMedias();
+        for(Media media: modelMedias){
+            if(media instanceof CheckOutItem){
+                medias.add(convertToDto((CheckOutItem)media));
+            }
+            if(media instanceof NonCheckOutItem){
+                medias.add(convertToDto((NonCheckOutItem)media));
+            }
+        }
+        return new OfflineDto(offline.getId(), offline.getAddress(), offline.getName(), offline.getAccountCategory(), offline.getIsLocal(), offline.getNumChecked(), events, medias);
     }
+    
     public static OnlineDto convertToDto(Online online){
         if(online==null) throw new IllegalArgumentException("Customer not found.");
-        HashSet<EventDto> howard = null;
-        HashSet<MediaDto> howard2 = null;
-        return new OnlineDto(online.getId(), online.getAddress(), online.getName(), online.getAccountCategory(), online.getIsLocal(), online.getNumChecked(), howard, howard2, online.getUsername(), online.getPassword(), online.getEmail());
+        HashSet<EventDto> events = new HashSet<EventDto>();
+        HashSet<Event> modelEvents = (HashSet)online.getEvents();
+        for(Event event: modelEvents){
+            events.add(convertToDto(event));
+        }
+
+        HashSet<MediaDto> medias = new HashSet<MediaDto>();
+        HashSet<Media> modelMedias = (HashSet)online.getMedias();
+        for(Media media: modelMedias){
+            if(media instanceof CheckOutItem){
+                medias.add(convertToDto((CheckOutItem)media));
+            }
+            if(media instanceof NonCheckOutItem){
+                medias.add(convertToDto((NonCheckOutItem)media));
+            }
+        }
+        return new OnlineDto(online.getId(), online.getAddress(), online.getName(), online.getAccountCategory(), online.getIsLocal(), online.getNumChecked(), events, medias, online.getUsername(), online.getPassword(), online.getEmail());
     }
 }
