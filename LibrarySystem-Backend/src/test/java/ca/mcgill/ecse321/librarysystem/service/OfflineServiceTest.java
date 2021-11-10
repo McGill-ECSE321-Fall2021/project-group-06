601407859f7d3308d4d68132589301c649c1dba9
DavidHu411 <david.hu@mail.mcgill.ca>
2021-11-09 21:07:45 -0500

    my test worked first try haha, what a miracle

---
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/service/OfflineServiceTest.java
@@ -0,0 +1,110 @@
+package ca.mcgill.ecse321.librarysystem.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import ca.mcgill.ecse321.librarysystem.dao.*;
+import ca.mcgill.ecse321.librarysystem.models.Offline;
+import ca.mcgill.ecse321.librarysystem.models.Account.AccountCategory;
+
+@ExtendWith(MockitoExtension.class)
+public class OfflineServiceTest {
+    @Mock
+    private AccountRepository accountDao;
+    @Mock
+    private EventRepository eventDao;
+    @Mock
+    private LibrarianRepository librarianDao;
+    @Mock 
+    private MediaRepository mediaDao;
+    @Mock 
+    private OpeningHourRepository openingHourDao; 
+    @Mock 
+    private ShiftRepository shiftDao;
+
+    @InjectMocks
+    private OfflineService offlineService;
+
+    private static final int OFFLINE_ID = 727;
+    private static final String OFFLINE_ADDRESS = "727 bluezenithhdhr street";
+    private static final String OFFLINE_NAME = "WhiteCat";
+    private static final AccountCategory OFFLINE_ACCOUNTCATEGORY = AccountCategory.Offline;
+    private static final boolean OFFLINE_ISLOCAL = true;
+    private static final int OFFLINE_ITEMSCHECKED = 1241;
+    
+    @BeforeEach
+    public void setMockOutput() {
+        lenient().when(accountDao.findAccountById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
+			if (invocation.getArgument(0).equals(OFFLINE_ID)) {
+				Offline offline = new Offline();
+				offline.setId(OFFLINE_ID);
+                offline.setAddress(OFFLINE_ADDRESS);
+                offline.setName(OFFLINE_NAME);
+                offline.setAccountCategory(OFFLINE_ACCOUNTCATEGORY);
+                offline.setIsLocal(OFFLINE_ISLOCAL);
+                offline.setNumChecked(OFFLINE_ITEMSCHECKED);
+				return offline;
+			} else {
+				return null;
+			}
+		});
+
+        // Whenever anything is saved, just return the parameter object
+		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+			return invocation.getArgument(0);
+		};
+		lenient().when(accountDao.save(any(Offline.class))).thenAnswer(returnParameterAsAnswer);
+		//lenient().when(eventDao.save(any(Event.class))).thenAnswer(returnParameterAsAnswer);
+		//lenient().when(registrationDao.save(any(Registration.class))).thenAnswer(returnParameterAsAnswer);
+    }
+
+    @Test
+	public void testCreateOffline() {
+		assertEquals(0, offlineService.getAllOfflines().size());
+        
+        int id = 727;
+        String address = "Badeu C-TYPEDTHRFL street";
+        String name = "RyuK";
+        AccountCategory accountCategory = AccountCategory.Offline;
+        boolean isLocal = true;
+        int numChecked = 4;
+        Offline offline = null;
+		try {
+			offline = offlineService.createOffline(id, address, name, accountCategory, isLocal, numChecked);
+
+		}catch(IllegalArgumentException e) {
+			fail();
+		}
+
+		assertNotNull(offline);
+		assertEquals(id, offline.getId());
+		assertEquals(address, offline.getAddress());
+        assertEquals(name, offline.getName());
+        assertEquals(accountCategory, offline.getAccountCategory());
+        assertEquals(isLocal, offline.getIsLocal());
+        assertEquals(numChecked, offline.getNumChecked());
+        //assertEquals(5, 6);
+	}
+
+}
--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/service/TestLibrarySystemService.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/service/TestLibrarySystemService.java
@@ -1,28 +1,41 @@
-package ca.mcgill.ecse321.librarysystem.service;
+// package ca.mcgill.ecse321.librarysystem.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.lenient;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertNotNull;
+// import static org.junit.jupiter.api.Assertions.assertNull;
+// import static org.junit.jupiter.api.Assertions.fail;
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.ArgumentMatchers.anyString;
+// import static org.mockito.Mockito.lenient;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
+// import java.sql.Date;
+// import java.sql.Time;
+// import java.time.LocalTime;
+// import java.time.format.DateTimeFormatter;
+// import java.util.Calendar;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import org.mockito.invocation.InvocationOnMock;
+// import org.mockito.junit.jupiter.MockitoExtension;
+// import org.mockito.stubbing.Answer;
 
-public class TestLibrarySystemService {
+// @ExtendWith(MockitoExtension.class)
+// public class TestLibrarySystemService {
+//     @Mock
+//     private AccountRepository accountRepository
+//     @Mock
+//     private EventRepository eventRepository
+//     @Mock
+//     private LibrarianRepository librarianRepository
+//     @Mock 
+//     private MediaRepository mediaRepository
+//     @Mock 
+//     private OpeningHourRepository openingHourRepository 
+//     @Mock 
+//     private ShiftRepository shiftRepository
     
-}
+// }
