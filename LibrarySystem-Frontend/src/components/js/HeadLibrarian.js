28e85b8cdd7acdb1e0f31e2b8ea99a0aa9ba51fd
DavidHu411 <david.hu@mail.mcgill.ca>
2021-11-25 01:12:24 -0500

    changes to HLjs and vue

--- LibrarySystem-Frontend/src/components/js/HeadLibrarian.js
+++ LibrarySystem-Frontend/src/components/js/HeadLibrarian.js
@@ -42,39 +42,39 @@ export default {
       }
     },
     created: function () {
-        // AXIOS.get('/create_openingHour/'.concat(id), {})
-        // .then(response => {
-        //   // JSON responses are automatically parsed.
-        //   this.openingHours = response.data
-        //   for (let x in response.data){
-        //     if(response.data[x].dayOfWeek == "Monday"){
-        //       this.Monday = response.data[x];
-        //     }
-        //   }
-        //   for (let x in response.data){
-        //     if(response.data[x].dayOfWeek == "Tuesday"){
-        //       this.Tuesday = response.data[x];
-        //     }
-        //   }
-        //   for (let x in response.data){
-        //     if(response.data[x].dayOfWeek == "Wednesday"){
-        //       this.Wednesday = response.data[x];
-        //     }
-        //   }
-        //   for (let x in response.data){
-        //     if(response.data[x].dayOfWeek == "Thursday"){
-        //       this.Thursday = response.data[x];
-        //     }
-        //   }
-        //   for (let x in response.data){
-        //     if(response.data[x].dayOfWeek == "Friday"){
-        //       this.Friday = response.data[x];
-        //     }
-        //   }
-        // })
-        // .catch(e => {
-        //   this.errorOH = e
-        // })
+        AXIOS.get('/openingHours')
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.openingHours = response.data
+          for (let x in response.data){
+            if(response.data[x].dayOfWeek == "Monday"){
+              this.Monday = response.data[x];
+            }
+          }
+          for (let x in response.data){
+            if(response.data[x].dayOfWeek == "Tuesday"){
+              this.Tuesday = response.data[x];
+            }
+          }
+          for (let x in response.data){
+            if(response.data[x].dayOfWeek == "Wednesday"){
+              this.Wednesday = response.data[x];
+            }
+          }
+          for (let x in response.data){
+            if(response.data[x].dayOfWeek == "Thursday"){
+              this.Thursday = response.data[x];
+            }
+          }
+          for (let x in response.data){
+            if(response.data[x].dayOfWeek == "Friday"){
+              this.Friday = response.data[x];
+            }
+          }
+        })
+        .catch(e => {
+          this.errorOH = e
+        })
     
     },
     methods: {
@@ -95,44 +95,42 @@ export default {
       //   })
       // },
       createShift: function(shiftID, dayOfWeek, startTime, endTime){
-        AXIOS.post('/create_headlibrarian', '/create_headlibrarian/',{
+        AXIOS.post('/createShift/'.concat(shiftID), {}, {
           params: {
-            shiftID: shiftID,
-            dayOfWeek: dayOfWeek,
-            startTime: startTime,
-            endTime: endTime,
+            DayOfWeek: dayOfWeek,
+            start: startTime,
+            end: endTime,
           }
         })
         .then(response => {
-          this.shifts.push(response.data)
-          this.errorShift = ''
-          this.newShift = ''
+          swal("Success", "Shift Was Created Successfully!", "success")
         })
         .catch(e => {
           this.errorShift = e
         })
       },
-        createOH: function (id,DayOfWeek,startTime,endTime) {
-            AXIOS.post('/create_openingHour/'.concat(id), {}, {
-              params: {
-                id: id,
-                DayOfWeek: DayOfWeek,
-                startTime: startTime,
-                endTime: endTime,
-              }
-            })
-            .then(response => {
-            // JSON responses are automatically parsed.
+      createOH: function (id,DayOfWeek,startTime,endTime) {
+          AXIOS.post('/create_openingHour/'.concat(id), {}, {
+            params: {
+              dayOfWeek: DayOfWeek,
+              startTime: startTime,
+              endTime: endTime,
+            }
+          })
+          .then(response => {
+          // JSON responses are automatically parsed.
 
-            //@niels i think this whole thing could just be empty cause you are getting all of OpeningHours in ur created function above
-            //not 100% sure, just keep it like this
-              this.openingHours.push(response.data)
-              this.errorOH = ''
-              this.newOH = ''
-            })
-            .catch(e => {
-              this.errorOH = e
-            })
-        }
+          //@niels i think this whole thing could just be empty cause you are getting all of OpeningHours in ur created function above
+          //not 100% sure, just keep it like this
+          // This whole thing bad, lol -David
+            // this.openingHours.push(response.data)
+            // this.errorOH = ''
+            // this.newOH = ''
+            swal("Success", "Opening Hour Was Created Successfully!", "success")
+          })
+          .catch(e => {
+            this.errorOH = e
+          })
+      }
     }
 }
