4e98c79418eaba8f07f89f001b5cf1e126a1dab6
Samuel Lin <lin.sam217@gmail.com>
2021-11-28 02:31:01 -0500

    Modified style for Online User Info

--- LibrarySystem-Frontend/src/components/Online.vue
+++ LibrarySystem-Frontend/src/components/Online.vue
@@ -1,42 +1,59 @@
 <template>
 
   <div id="Online">
-    <table>
+    <h1>User Online Account</h1>
+      <div class="container">
+    <div class="column">
+    <table class="Account_Table">
       <tr v="Account">
-        <td>
-          <p style="font-size:30px">
-            Personal information
-          </p>
+        <th style="font-size:30px" colspan=2>Personal Information</th>
+      </tr>
+      <tr>
+        <td class="Account_Table_left">
+          <p style="font-size:20px">ID Number</p>
+        </td>
+        <td class="Account_Table_right">
+          <p style="font-size:20px">{{ Account.id }} </p>
         </td>
       </tr>
       <tr>
-        <td>
-          {{ Account.id }}
+        <td class="Account_Table_left">
+          <p style="font-size:20px">Name</p>
+        </td>
+        <td class="Account_Table_right"> 
+          <p style="font-size:20px">{{ Account.name }} </p>
         </td>
       </tr>
       <tr>
-        <td>
-          {{ Account.name }}
+        <td class="Account_Table_left">
+          <p style="font-size:20px">Local Account?</p>
+        </td>
+        <td class="Account_Table_right"> 
+          <p style="font-size:20px">{{ Account.isLocal }} </p>
         </td>
       </tr>
       <tr>
-        <td>
-          {{ Account.isLocal }}
+        <td class="Account_Table_left">
+          <p style="font-size:20px">Email Address</p>
+        </td>
+        <td class="Account_Table_right"> 
+          <p style="font-size:20px">{{ Account.email }} </p>
+        </td>        
+      </tr>
+      <tr>
+        <td class="Account_Table_left">
+          <p style="font-size:20px">Home Address</p>
+        </td>
+        <td class="Account_Table_right"> 
+          <p style="font-size:20px">{{ Account.address }} </p>
         </td>
       </tr>
       <tr>
-        <td>
-          {{ Account.numChecked }}
+        <td class="Account_Table_left">
+          <p style="font-size:20px">Number of Checked Out Media</p>
         </td>
-      </tr>
-      <tr>
-        <td>
-          {{ Account.email }}
-        </td>
-      </tr>
-      <tr v-for="event in bookedEvent" :key="event.name">
-        <td>
-          {{event.name}}
+        <td class="Account_Table_right"> 
+          <p style="font-size:20px">{{ Account.numChecked }} </p>
         </td>
       </tr>
       <tr v-for="media in checkedOutMedias" :key="media.name">
@@ -44,42 +61,48 @@
           {{name}}
         </td>
       </tr>
-      <tr>
+      <tr v-for="event in bookedEvent" :key="event.name">
         <td>
-          {{ Account.address }}
+          {{event.name}}
         </td>
       </tr>
     </table>
-    <table>
+    </div>
+    <table class="UpdateAcc">
       <tr>
         <td>
-          <p style="font-size:30px">
+          <p style="font-size:30px;font-weight:bold">
             Update Personal information
           </p>
         </td>
       </tr>
       <tr>
         <td>
-          <input type="text" v-model="onlineName" placeholder="update online name">
+          <p style="font-size:20px">Update Account Name</p>
+          <input type="text" v-model="onlineName" placeholder="New Name">
         </td>
       </tr>
       <tr>
         <td>
-          <input type="text" v-model="onlinePassword" placeholder="update online password">
+          <p style="font-size:20px">Update Password</p>
+          <input type="text" v-model="onlinePassword" placeholder="New Password">
         </td>
       </tr>
       <tr>
         <td>
-          <input type="text" v-model="onlineAddress" placeholder="update online address">
+          <p style="font-size:20px">Update Home Address</p>
+          <input type="text" v-model="onlineAddress" placeholder="New Home Address">
         </td>
       </tr>
       <tr>
         <td>
-          <button v-bind:disabled="!onlineName || !onlinePassword || !onlineAddress" @click="updateInfo(onlineName,onlinePassword,onlineAddress)"> update personal information </button>
+          <button v-bind:disabled="!onlineName || !onlinePassword || !onlineAddress" @click="updateInfo(onlineName,onlinePassword,onlineAddress)"> Update Personal Information </button>
         </td>
       </tr>
     </table>
-    <table>
+    </div>
+    <div class="column">
+    <table class="Media">
       <tr>
         <td>
           <p style="font-size:30px">
@@ -118,22 +141,74 @@
     <p>
       <span v-if="errorEvent" style="color:red">Error: {{errorEvent}} </span>
     </p>
+    </div>
   </div>
 </template>
 <script src="./js/BookEvent.js">
 </script>
+<style scoped>
+/* @import '/assets/design.scss'; */
+</style>
 <style>
+/* @import '/assets/design.scss'; */
   #Online {
     min-height: 800px;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     color: #2c3e50;
     /* background: #f2ece8; */
   }  
-  table{
+  table.Account_Table{
+    min-height: 300px;
+    min-width: 500px;
+    max-width: 500px;
+    text-align:center;
+    /*float: left; */
+    border-collapse: collapse;
+
+    background: rgba(255, 255, 255, 0.06);
+    border-radius: 16px;
+    /*box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);*/
+    backdrop-filter: blur(3.2px);
+  }
+  table.UpdateAcc{
     min-height: 300px;
     text-align:center;
-    float: left;
+    /*float: left; */
+    border-spacing: 1px;
+    border-collapse: separate;
+
+    background: rgba(255, 255, 255, 0.06);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(3.2px);
   }
+  td.Account_Table_left{
+    border:1px solid black;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    vertical-align: middle;
+    min-width: 50px;
+    max-width: 50px;
+  }
+  td.Account_Table_right{
+    border:1px solid black;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    vertical-align: middle;
+    min-width: 100px;
+    max-width: 100px;
+  }
+  td{
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  
 #scrollBox {
   border: 1px solid black;
   width: 300px;
@@ -147,4 +222,14 @@
   body{
     background: #fff1e6
   }
+.column {
+  float: left;
+  vertical-align: 50%;
+  width: 33%;
+  padding: 10px;
+}
+.container {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
