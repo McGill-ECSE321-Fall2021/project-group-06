28dc14a6e95d47ccd1b979b61b0b1d3815df1d26
DavidHu411 <david.hu@mail.mcgill.ca>
2021-12-04 03:02:46 -0500

    attempt2

--- LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/MainActivity.java
+++ LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/MainActivity.java
@@ -3,6 +3,8 @@ package ca.mcgill.ecse321.librarysystem_android;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,12 +16,17 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import ca.mcgill.ecse321.librarysystem_android.databinding.ActivityMainBinding;
+import cz.msebera.android.httpclient.Header;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String error = null;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -72,5 +79,49 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void addEvent(View v) {
+        error = "";
+        RequestParams rq = new RequestParams();
+        final TextView eventName = (TextView) findViewById(R.id.newperson_name);
+        final TextView eventDate = (TextView) findViewById(R.id.newperson_date);
+        final TextView eventStart = (TextView) findViewById(R.id.newperson_start);
+        final TextView eventEnd = (TextView) findViewById(R.id.newperson_end);
+        rq.put("eventDate", eventDate.getText().toString());
+        rq.put("eventStart", eventStart.getText().toString());
+        rq.put("eventEnd", eventEnd.getText().toString());
+        HttpUtils.post("create_event/" + eventName.getText().toString(), rq, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                eventName.setText("");
+                eventDate.setText("");
+                eventStart.setText("");
+                eventEnd.setText("");
+
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    //error += HttpUtils.getAbsoluteUrl("persons/" + tv.getText().toString());
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
     }
 }
