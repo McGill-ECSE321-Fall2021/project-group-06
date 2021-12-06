d8de885e77b92c0853c94840d4a25c935ca8c764
Samuel Lin <lin.sam217@gmail.com>
2021-12-06 06:26:50 -0500

    Added Register and View "stuff" buttons to login page
    Need to change "local" to spinner.

--- LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/Login.java
+++ LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/Login.java
@@ -41,8 +41,13 @@ public class Login extends AppCompatActivity{
 
     private String error = null;
     private Button loginButton;
+    private Button mediaButton;
+    private Button eventButton;
+    private Button ohButton;
     private EditText loginID;
     private EditText loginPassword;
+    private Button registerButton;
+    // Add more EditText private variables if needed
 
     private static final String HEADER = "User Login";
 
@@ -54,11 +59,43 @@ public class Login extends AppCompatActivity{
         loginID = (EditText) findViewById(R.id.user_loginID);
         loginPassword = (EditText) findViewById(R.id.user_loginPwd);
         loginButton = (Button) findViewById(R.id.login_button);
+        mediaButton = (Button) findViewById(R.id.media_button);
+        eventButton = (Button) findViewById(R.id.event_button);
+        ohButton = (Button) findViewById(R.id.oh_button);
 
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 loginUser();
+            }
+        });
+
+        mediaButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                getToMediaPage();
+            }
+        });
+
+        eventButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                getToEventsPage();
+            }
+        });
+
+        ohButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                getToOHPage();
+            }
+        });
+
+        registerButton = (Button) findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                registerUser();
             }
         });
 
@@ -68,6 +105,25 @@ public class Login extends AppCompatActivity{
 
     public void loginUser(){
 
+    }
+
+    public void registerUser(){
+
+    }
+
+    public void getToMediaPage(){
+        Intent intent = new Intent(this, MainActivity.class); // Might need to be changed to Media.java
+        startActivity(intent);
+    }
+
+    public void getToEventsPage(){
+        Intent intent = new Intent(this, MainActivity.class); // Might need to be changed to Event.java
+        startActivity(intent);
+    }
+
+    public void getToOHPage(){
+        Intent intent = new Intent(this, MainActivity.class); // Might need to be changed to OpeningHour.java
+        startActivity(intent);
     }
 
     private void refreshErrorMessage(){
