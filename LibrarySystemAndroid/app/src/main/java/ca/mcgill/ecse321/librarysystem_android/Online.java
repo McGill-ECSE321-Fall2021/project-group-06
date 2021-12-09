bd40b690823f7390813b77576fc3a84abd0eede4
Samuel Lin <lin.sam217@gmail.com>
2021-12-09 14:32:02 -0500

    Cleaned Online and changed Http mapping to right ones. Added onClick in online.xml

--- LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/Online.java
+++ LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/Online.java
@@ -1,12 +1,9 @@
 package ca.mcgill.ecse321.librarysystem_android;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -23,8 +20,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,6 +28,7 @@ public class Online extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    // Initializing Online page with data from server
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,7 +81,7 @@ public class Online extends AppCompatActivity {
     }
     private void refreshErrorMessage() {
         // set the error message
-        TextView tvError = (TextView) findViewById(R.id.error);
+        TextView tvError = findViewById(R.id.error);
         tvError.setText(error);
 
         if (error == null || error.length() == 0) {
@@ -96,19 +92,25 @@ public class Online extends AppCompatActivity {
     }
 
     /**
-     * Creates an event
-     * @param v
+     * addEvent
+     * Creates an event in current view
+     * @param v: current view
      */
     public void addEvent(View v) {
         error = "";
+
+        // Retrieve all fields written by user in "Book Event" section and add those as requested parameters
         RequestParams rq = new RequestParams();
-        final TextView eventName = (TextView) findViewById(R.id.event_name);
-        final TextView eventDate = (TextView) findViewById(R.id.event_date);
-        final TextView eventStart = (TextView) findViewById(R.id.event_start);
-        final TextView eventEnd = (TextView) findViewById(R.id.event_end);
+        final TextView eventName = findViewById(R.id.event_name);
+        final TextView eventDate = findViewById(R.id.event_date);
+        final TextView eventStart = findViewById(R.id.event_start);
+        final TextView eventEnd = findViewById(R.id.event_end);
         rq.put("eventDate", eventDate.getText().toString());
         rq.put("eventStart", eventStart.getText().toString());
         rq.put("eventEnd", eventEnd.getText().toString());
+
+        // REST Service call POST with Http: creates an Event on success (see EventController.java in backend)
+        // On failure: displays error message on top of screen of current view
         HttpUtils.post("/create_event/" + eventName.getText().toString(), rq, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -122,7 +124,6 @@ public class Online extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 try {
-                    //error += HttpUtils.getAbsoluteUrl("persons/" + tv.getText().toString());
                     error += errorResponse.get("message").toString();
                 } catch (JSONException e) {
                     error += e.getMessage();
@@ -133,20 +134,26 @@ public class Online extends AppCompatActivity {
     }
 
     /**
+     * updateInfo
      * Updates Online Account information
      * If some fields are to remain unchanged, reenter previously entered information
-     * @param v
+     * @param v: current view
      */
     public void updateInfo(View v) {
         error = "";
+
+        // Retrieve all fields written by user in "Update Info" section and add those as requested parameters
         RequestParams rq = new RequestParams();
-        final TextView accountID = (EditText) findViewById(R.id.account_to_update);
-        final TextView addr = (EditText) findViewById(R.id.updated_address);
-        final TextView name = (EditText) findViewById(R.id.updated_name);
-        final TextView pwd = (EditText) findViewById(R.id.updated_password);
+        final TextView accountID = findViewById(R.id.account_to_update);
+        final TextView addr = findViewById(R.id.updated_address);
+        final TextView name = findViewById(R.id.updated_name);
+        final TextView pwd = findViewById(R.id.updated_password);
         rq.put("address", addr.getText().toString());
         rq.put("name", name.getText().toString());
         rq.put("password", pwd.getText().toString());
+
+        // REST Service call POST with Http: updates Online account on success (see OnlineController.java in backend)
+        // On failure: displays error message on top of screen of current view
         HttpUtils.post("edit_online/" + accountID.getText().toString(), rq, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -159,32 +166,6 @@ public class Online extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 try {
-                    //error += HttpUtils.getAbsoluteUrl("persons/" + tv.getText().toString());
-                    error += errorResponse.get("message").toString();
-                } catch (JSONException e) {
-                    error += e.getMessage();
-                }
-                refreshErrorMessage();
-            }
-        });
-    }
-    public void reserveMedia(View v) {
-        error = "";
-        RequestParams rq = new RequestParams();
-        final TextView accountID = (EditText) findViewById(R.id.userID);
-        final TextView mediaID = (EditText) findViewById(R.id.mediaID);
-        rq.put("userId", accountID.getText().toString());
-        HttpUtils.post("reserve_media_online/" + mediaID.getText().toString(), rq, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                refreshErrorMessage();
-                accountID.setText("");
-                mediaID.setText("");
-            }
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                try {
-                    //error += HttpUtils.getAbsoluteUrl("persons/" + tv.getText().toString());
                     error += errorResponse.get("message").toString();
                 } catch (JSONException e) {
                     error += e.getMessage();
@@ -195,8 +176,44 @@ public class Online extends AppCompatActivity {
     }
 
     /**
-     * Set content view back to main
-     * @param v
+     * reserveMedia
+     * Reserve media item from Online account
+     * @param v: current view
+     */
+    public void reserveMedia(View v) {
+        error = "";
+
+        // Retrieve all fields written by user in "Reserve Media" section and add those as requested parameters
+        RequestParams rq = new RequestParams();
+        final TextView accountID = findViewById(R.id.userID);
+        final TextView mediaID = findViewById(R.id.mediaID);
+        rq.put("userId", accountID.getText().toString());
+
+        // REST Service call PUT with Http: reserves Media item to Online account on success (see OnlineController.java in backend)
+        // On failure: displays error message on top of screen of current view
+        HttpUtils.put("reserve_media_online/" + mediaID.getText().toString(), rq, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                accountID.setText("");
+                mediaID.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+    /**
+     * returnToMain
+     * Set current content view back to main page (Login page)
+     * @param v: current view
      */
     public void returnToMain(View v) {
         error="";
