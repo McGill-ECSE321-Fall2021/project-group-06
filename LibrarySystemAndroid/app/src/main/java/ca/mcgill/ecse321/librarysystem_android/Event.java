baf85eea2e67092bece3857cc9ccfd5ed2e83cb5
Niels <77604434+ballofsunshine@users.noreply.github.com>
2021-12-09 16:14:43 -0500

    Moved the HttpUtils to their respective class

--- LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/Event.java
+++ LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/Event.java
@@ -6,16 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.widget.Button;
+import android.widget.TextView;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
 
 
 public class Event extends AppCompatActivity {
+    private String error;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event);
 
+        getAllEvents();
         configureHomeButton();
     }
 
@@ -25,6 +37,56 @@ public class Event extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+    }
+    /**
+     * getAllEvents
+     * Gets all events from database to current view
+     * @param v: current view
+     */
+    public void getAllEvents() {
+        error = "";
+        RequestParams rq = new RequestParams();
+
+        // REST service call GET with Http: gets all Events and writes details on Event page on success
+        // On failure: displays error message on top of screen of current view
+        HttpUtils.get("events/getAllEvents/", rq, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONArray response) {
+                final JSONArray[] allOP= {new JSONArray()};
+                allOP[0]=response;
+                JSONObject object;
+                try {
+                    for(int i=0; i<allOP[0].length(); i++) {
+                        object = allOP[0].getJSONObject(i);
+                        String name= object.getString("name");
+                        String date=String.valueOf(object.get("date"));
+                        String start=String.valueOf(object.get("eventStart"));
+                        String end=String.valueOf(object.get("eventEnd"));
+
+                        TextView tvName = findViewById(R.id.evt_name);
+                        TextView tvDate = findViewById(R.id.evt_date);
+                        TextView tvStart = findViewById(R.id.evt_start);
+                        TextView tvEnd = findViewById(R.id.evt_end);
+                        tvDate.setText(date);
+                        tvStart.setText(start);
+                        tvEnd.setText(end);
+                        tvName.setText(name);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                setContentView(R.layout.event);
+
+            }
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
             }
         });
     }
--- LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/OpeningHours.java
+++ LibrarySystemAndroid/app/src/main/java/ca/mcgill/ecse321/librarysystem_android/OpeningHours.java
@@ -8,14 +8,25 @@ import android.view.View;
 
 
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class OpeningHours extends AppCompatActivity {
+    private String error;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.opening_hour);
 
+        getAllOpeningHours();
         configureHomeButton();
     }
 
@@ -25,6 +36,83 @@ public class OpeningHours extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+    }
+    /**
+     * getAllOpeningHours
+     * Get all opening hours from database and write to OH view, calling from current view
+     * @param v: current view
+     */
+    public void getAllOpeningHours(){
+        error = "";
+        RequestParams rq = new RequestParams();
+
+        // REST service call GET with Http: gets all OH and writes details on OH page on success
+        // On failure: displays error message on top of screen of current view
+        HttpUtils.get("openingHours/", rq, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONArray response) {
+
+                try {
+                    for(int i=0; i<response.length(); i++) {
+                        JSONObject object = response.getJSONObject(i);
+                        String date= object.getString("DayOfWeek");
+                        String start= object.getString("startTime");
+                        String end= object.getString("endTime");
+
+                        if (date.equals("Monday")) {
+                            TextView tvDate= findViewById(R.id.oh_date_monday);
+                            TextView tvStart= findViewById(R.id.oh_start_time_monday);
+                            TextView tvEnd= findViewById(R.id.oh_end_time_monday);
+                            tvDate.setText(date);
+                            tvStart.setText(start);
+                            tvEnd.setText(end);
+                        }
+                        if (date.equals("Tuesday")) {
+                            TextView tvDate = findViewById(R.id.oh_date_tuesday);
+                            TextView tvStart = findViewById(R.id.oh_start_time_tuesday);
+                            TextView tvEnd = findViewById(R.id.oh_end_time_tuesday);
+                            tvDate.setText(date);
+                            tvStart.setText(start);
+                            tvEnd.setText(end);
+                        }
+                        if (date.equals("Wednesday")) {
+                            TextView tvDate = findViewById(R.id.oh_date_wednesday);
+                            TextView tvStart = findViewById(R.id.oh_start_time_wednesday);
+                            TextView tvEnd = findViewById(R.id.oh_end_time_wednesday);
+                            tvDate.setText(date);
+                            tvStart.setText(start);
+                            tvEnd.setText(end);
+                        }
+                        if (date.equals("Thursday")) {
+                            TextView tvDate = findViewById(R.id.oh_date_thursday);
+                            TextView tvStart = findViewById(R.id.oh_start_time_thursday);
+                            TextView tvEnd = findViewById(R.id.oh_end_time_thursday);
+                            tvDate.setText(date);
+                            tvStart.setText(start);
+                            tvEnd.setText(end);
+                        }
+                        if (date.equals("Friday")) {
+                            TextView tvDate = findViewById(R.id.oh_date_friday);
+                            TextView tvStart = findViewById(R.id.oh_start_time_friday);
+                            TextView tvEnd = findViewById(R.id.oh_end_time_friday);
+                            tvDate.setText(date);
+                            tvStart.setText(start);
+                            tvEnd.setText(end);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
             }
         });
     }
